63c80aa823aeb42f68056b2f8e55801c3165c144
Zeel Gajrawala <zeel.gajrawala.work@gmail.com>
2023-07-13 18:56:03 +0530

    Corrected env files and added necessary config in angular.json to replace file during build

--- client/src/app/core/services/auth/auth.service.ts
+++ client/src/app/core/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { User } from '../../models/user';
 
 @Injectable({
--- client/src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    API_ENDPOINT: 'https://chat-app-ow0i.onrender.com'
-};
